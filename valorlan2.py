f6cf01ef2be98857004d083c7e7235b0abc67afa
Pedro Lourega <pedrolourega1@gmail.com>
2024-10-26 16:18:09 -0300

    update-lancode2
    adcionei horas e minutos no sistema.

--- valorlan2.py
+++ valorlan2.py
@@ -1,14 +1,15 @@
-#Função principal
-def  calcular_valor(duracao_minutos, preco_fixo_hora, preco_fixo_30min):
-
+#Main
+def calcular_valor(duracao_minutos, preco_fixo_hora, preco_fixo_30min):
+    
+    #Verifica a duracao de até 30m
     if duracao_minutos <= 30:
         return preco_fixo_30min
 
+    #Conversor de duracao
     horas = duracao_minutos // 60
-
     valor = preco_fixo_hora * horas
 
-    
+    #Desconto
     if horas > 5:
         desconto = 0.15
     elif horas > 3:
@@ -16,14 +17,15 @@ def  calcular_valor(duracao_minutos, preco_fixo_hora, preco_fixo_30min):
     elif horas > 2:
         desconto = 0.10
     elif horas > 1:
-        desconto = 0.7
+        desconto = 0.07  
     else:
         desconto = 0.0
+
 
     valor_com_desconto = valor * (1 - desconto)
     return valor_com_desconto
 
-#Funções para cada serviço
+#Preço de cada serviço disponivel
 def calcular_valor_lan_house(duracao_minutos):
     return calcular_valor(duracao_minutos, preco_fixo_hora=30, preco_fixo_30min=20)
 
@@ -33,21 +35,26 @@ def calcular_valor_lan_ps5(duracao_minutos):
 def calcular_valor_lan_vr(duracao_minutos):
     return calcular_valor(duracao_minutos, preco_fixo_hora=100, preco_fixo_30min=50)
 
-#Coletor de entrada e saída da lan
-hora_entrada = list(map(int, input("Digite a hora e o minuto de entrada (HH MM):").split()))
-hora_saida = list(map(int, input("Digite a hora e o minuto de entrada (HH MM):").split()))
+#Entrada de dados de tempo utilizado
+hora_entrada = list(map(int, input("Digite a hora e o minuto de entrada (HH MM): ").split()))
+hora_saida = list(map(int, input("Digite a hora e o minuto de saída (HH MM): ").split()))
 
-#Converte as horas de entrada e saída
+#Converte a entrada e a saída
 entrada_minutos = hora_entrada[0] * 60 + hora_entrada[1]
 saida_minutos = hora_saida[0] * 60 + hora_saida[1]
 
-#Calcula a duração em minutos
+#Transforma em minutos
 duracao_minutos = saida_minutos - entrada_minutos
 
-#Pergunta para o usuário
-tipo_servico = input("Digite o tipo de serviço (LanHouse, PS5, VR): ").lower()
+#Calculo de horas e minutos
+horas_totais = duracao_minutos // 60
+minutos_totais = duracao_minutos % 60
 
 
+#Pergunta o serviço
+tipo_servico = input("Digite o tipo de serviço (lanhouse, ps5, vr): ").lower()
+
+#Faz o calculo do valor
 if tipo_servico == 'lanhouse':
     valor = calcular_valor_lan_house(duracao_minutos)
 elif tipo_servico == 'ps5':
@@ -55,9 +62,9 @@ elif tipo_servico == 'ps5':
 elif tipo_servico == 'vr':
     valor = calcular_valor_lan_vr(duracao_minutos)
 else:
-    valor = "Serviço inválido!" #Error!
+    valor = "Serviço inválido!"  #Erro!
 
-
-#Interface de resultado
-print(f"Duração total: {duracao_minutos} minutos")
-print(f"Valor a pagar: R$ {valor:2.f}" if isinstance(valor,(int, float))else valor)
+#Duração
+print(f"Duraçãoem minutos: {duracao_minutos} ")
+print(f"Duração total: {horas_totais}h:{minutos_totais}m ")
+print(f"Valor a pagar: R$ {valor:.2f}" if isinstance(valor, (int, float)) else valor)
